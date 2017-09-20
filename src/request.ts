0f157c797a09d1d6a9e3084c7e86fb6f7dc68ae3
Krzysztof Rzymkowski <krzysztof.rzymkowski@lingaro.com>
2017-09-20 17:22:43 +0200

    server url definitions in package.json

--- src/request.ts
+++ src/request.ts
@@ -1,13 +1,15 @@
-import rest, { Request } from "rest";
+import rest, {Request} from "rest";
 import mime from "rest/interceptor/mime";
+import pkg from "../package.json";
 
-export const { URL } = process.env;
+const {TARGET} = process.env;
+export const URL = pkg['servers'][TARGET];
 
 export const client = (req: string | Request) => rest.wrap(mime)(
-  (typeof (req) === 'string'
-    ? (URL + req)
-    : Object.assign({}, req, { path: URL + req.path })
-  ) as any,
+    (typeof (req) === 'string'
+            ? (URL + req)
+            : Object.assign({}, req, {path: URL + (req as Request).path})
+    ) as any,
 );
 
 export default (req: string | Request) => client(req).then(response => response.entity);
--- src/sample.test.ts
+++ src/sample.test.ts
@@ -3,6 +3,5 @@ import request from "./request";
 
 test("sample test", async () => {
   const resp = await request("/users");
-  expect(resp[0].username).toBe("Bret");
   expect(resp).toMatchSnapshot();
 });
